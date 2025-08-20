ecfc2cb70008e29766b8986cef3ee9f0a79f564c
FengLingYu_7563 <162894213+FengLingYu7563@users.noreply.github.com>
2025-08-21 00:10:11 +0800

    cloud run ver.

--- main.py
+++ main.py
@@ -48,9 +48,7 @@ async def on_ready():
     """當機器人啟動時，同步斜線指令"""
     print(f"✅ 目前登入身份 --> {bot.user}")
     try:
-        # 添加斜線指令群組
         bot.tree.add_command(info_group)
-        # 同步斜線指令
         slash = await bot.tree.sync()
         print(f"✅ 載入 {len(slash)} 個斜線指令")
     except Exception as e:
